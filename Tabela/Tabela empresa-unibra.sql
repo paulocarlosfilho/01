e4d5c3031992a20c74e9217824b5351f50aa6169
paulocarlosfilho <83230882+paulocarlosfilho@users.noreply.github.com>
2021-04-30 22:34:23 -0300

    Add files via upload

---
+++ Tabela/Tabela empresa-unibra.sql
@@ -0,0 +1,29 @@
+create table empresa (
+empresa varchar(100) not null
+) default charset = utf8;
+
+insert into empresaunibrascreated_at values
+('Situação: Ativa'),
+('Número do CNPJ: 07.397.220/001-40'),
+('Razão Social: Ibgm - Instituto Brasileiro De Gestao & Marketing Ltda'),
+('Nome Fantasia: Unibra'),
+('Data de Abertura: 24/05/2005'),
+('Capital Social: R$ 938.700,00'),
+('Tipo: MATRIZ'),
+('Natureza Jurídica: Sociedade Empresária Limitada'),
+('Porte da Empresa: DEMAIS'),
+('Qnt. de Funcionários: Indeterminada'),
+('Faturamento Presumido: Indeterminado'),
+('Telefone: (81) 3222-3547'),
+('E-mail: dev@grupounibra.com'),
+('Estado / UF: Pernambuco / PE'),
+('Município: Recife'),
+('Logradouro: Rua Joaquim Felipe'),
+('Número: 250'),
+('Complemento:'),
+('CEP: 50.050-365'),
+('Atividade econômica: 8532-5/00 :: Educação superior - graduação e pós-graduação');
+
+SELECT * FROM empresa;
+
+drop table 
