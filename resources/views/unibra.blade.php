4e4f489ca25986c2908f7251323d7454791ade10
paulocarlosfilho <paulocarlosfilho@gmail.com>
2021-04-30 22:17:08 -0300

    Teste First Commite

---
+++ resources/views/unibra.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unibra</title>
+</head>
+<body>
+    <h1>Unibra</h1>
+        <ul>
+            <p>Situação: Ativa</p>
+            <p>Número do CNPJ: 07.397.220/001-40</p>
+            <p>Razão Social: Ibgm - Instituto Brasileiro De Gestao & Marketing Ltda</p>
+            <p>Nome Fantasia: Unibra</p>
+            <p>Data de Abertura: 24/05/2005</p>
+            <p>Capital Social: R$ 938.700,00</p>
+            <p>Tipo: MATRIZ</p>
+            <p>Natureza Jurídica: Sociedade Empresária Limitada</p>
+            <p>Porte da Empresa: DEMAIS</p>
+            <p>Qnt. de Funcionários: Indeterminada</p>
+            <p>Faturamento Presumido: Indeterminado</p>
+            <p>Telefone: (81) 3222-3547</p>
+            <p>E-mail: dev@grupounibra.com</p>
+            <p>Estado / UF: Pernambuco / PE</p>
+            <p>Município: Recife</p>
+            <p>Bairro: Boa Vista</p>
+            <p>Logradouro: Rua Joaquim Felipe</p>
+            <p>Número: 250</p>
+            <p>Complemento:</p>
+            <p>CEP: 50.050-365</p>
+            <p>Atividade econômica: 8532-5/00 :: Educação superior - graduação e pós-graduação</p>
+        </ul>
+    
+    
+</body>
+</html>
